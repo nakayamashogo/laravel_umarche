c1db0b15738fe26309a808147bbeedc3fef27a9d
nakayamashogo <nakayamaakg@gmail.com>
2022-07-23 21:04:35 +0900

    変数

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,6 +1,7 @@
 <x-tests.app>
   <x-slot name="header">ヘッダー1</x-slot>
   コンポーネントテスト1
-  <x-tests.card title="タイトル1" content="コンテント1" message="本文" />
+  <x-tests.card title="タイトル1" content="コンテント1" :message="$message" />
+
 
 </x-tests.app>
