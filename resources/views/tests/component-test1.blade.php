c23660608dfe0f3b95bee4914d0b2430e6b6ac06
nakayamashogo <nakayamaakg@gmail.com>
2022-07-23 23:21:02 +0900

    属性バッグ

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -2,6 +2,7 @@
   <x-slot name="header">ヘッダー1</x-slot>
   コンポーネントテスト1
   <x-tests.card title="タイトル1" content="コンテント1" :message="$message" />
-  <x-tests.card title="タイトル" />
+  <x-tests.card title="タイトル2" />
+  <x-tests.card title="CSSを変更したい" class="bg-red-300" />
 
 </x-tests.app>
